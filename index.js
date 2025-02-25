0591fdfc2d5e3221c2328ff2ae6d10720b5598f8
vsunholy <vitalijsnopov@gmail.com>
2025-02-25 12:02:14 +0200

    refactor data generation and save functionality; add user, product, category, and company generation with JSON and CSV output

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
-import { faker } from '@faker-js/faker';
+const { faker } = require('@faker-js/faker');
+const fs = require('fs');
 
-// const randomName = faker.person.fullName(); 
-// const randomFirstName = faker.person.firstName(); 
-// const randomSurname = faker.person.lastName();
+// generate data
 
-// console.log(randomName)
-// console.log(randomFirstName)
-// console.log(randomSurname)
-// sugeneruoti 100 netikru users/people su siais duomenimias: fist name,surname,phone,email,password,birthdate
-// su faker js
-
-const user = {
-    "firstName": faker.person.firstName(),
-    "surname": faker.person.lastName(),
-    "phone": faker.phone.number(),
-    "email": faker.internet.email(),
-    "username": faker.internet.username(),
-    "password": faker.internet.password(),
-    "birthdate": faker.date.birthdate()
+const generateUser = _ => {
+    return {
+        "firstName": faker.person.firstName(),
+        "surname": faker.person.lastName(),
+        "phone": faker.phone.number(),
+        "email": faker.internet.email(),
+        "username": faker.internet.username(),
+        "password": faker.internet.password(),
+        "birthdate": faker.date.birthdate()
+    }
+}
+const generateProduct = _ => {
+    return {
+        "title": faker.commerce.productName(),
+        "description": faker.commerce.productDescription(),
+        "price": faker.commerce.price(),
+        "category": faker.commerce.department(),
+    }
 }
 
-const product= {
-    "title": faker.commerce.productName(),
-    "description": faker.commerce.productDescription(),
-    "price": faker.commerce.price(),
-    "category": faker.commerce.department(),
+const generateCategory = _ => {
+    return {
+        "title": faker.commerce.department(),
+        "description": faker.commerce.productDescription(),
+    }
+}
+
+const generateCompany = _ => {
+    return{
+        "title": faker.company.name(),
+    }
+}
+
+
+const users = Array.from({ length: 1 }, generateUser);
+const products = Array.from({ length: 2 }, generateProduct);
+const categories = Array.from({ length: 3 }, generateCategory);
+const companies = Array.from({ length: 4 }, generateCompany);
+
+// save data to file
+
+fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
+fs.writeFileSync('products.json', JSON.stringify(products, null, 2));
+fs.writeFileSync('categories.json', JSON.stringify(categories, null, 2));
+fs.writeFileSync('companies.json', JSON.stringify(companies, null, 2));
+
+
+// save to csv file
+// const json2csv = require('json2csv').parse;
+// const csv = json2csv(users)
+// const csv1 = json2csv(products)
+// const csv2 = json2csv(categories)
+// const csv3 = json2csv(companies)
+// fs.writeFileSync('products.csv', csv);
+// fs.writeFileSync('products.csv', csv1);
+// fs.writeFileSync('categories.csv', csv2);
+// fs.writeFileSync('companies.csv', csv3);
+
+const csvHeader = Object.keys(users[0]).join(',') + '\n';
+let csvRows = '';
+for (let i = 0; i < users.length; i++) {
+    let csvRow = Object.values(users[i]).join(',') + '\n';
+    csvRows += csvRow;
     
 }
-
-
-
-
-
-
-
-
-
-
-
-console.log(product);
-console.log(user);
-
+fs.writeFileSync('users.csv', csvHeader + csvRows);
